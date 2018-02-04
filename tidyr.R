1a7f56cdc658f8ba1865f96eb30cab7168aac773
andyblueyo <abchen@uw.edu>
2018-02-03 20:21:59 -0800

    add bar charts

--- tidyr.R
+++ tidyr.R
@@ -3,6 +3,8 @@
 library(dplyr)
 #install.packages("tidyr")
 library(tidyr)
+#install.packages("ggplot2")
+library(ggplot2)
 
 ## Create the messy fake data set about ice cream
 flavors <- c("chocolate", "vanilla", "phish food", "ube", "strawberry", "guava sorbet")
@@ -12,8 +14,17 @@ sanfran.cost <- c(10, 12, 15, 17, 11, 21)
 
 ice.cream <- data.frame(flavors, seattle.cost, portland.cost, sanfran.cost)
 
+## Bar chart of ice cream prices in Seattle
+ggplot(ice.cream, aes(flavors, seattle.cost)) + geom_bar(stat = "identity")
+
 ## Gather the data into a more tidy form
 gather.data <- ice.cream %>% gather(location, price, seattle.cost, portland.cost, sanfran.cost)
 
+## Stacked bar chart of ice cream price and flavors
+ggplot(gather.data, aes(flavors, price)) + geom_bar(stat = "identity", aes(fill = location))
+
 ## Spread the data
 spread.data <- gather.data %>% spread(flavors, price)
+
+## Bar chart of prices of chocolate in diffrent locations
+ggplot(spread.data, aes(location, chocolate)) + geom_bar(stat = "identity")
