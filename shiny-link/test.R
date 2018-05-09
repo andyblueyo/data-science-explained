90903eb4b789fdbb0b149f330afc3dd09c9bcaf3
andyblueyo <abchen@uw.edu>
2018-05-09 02:12:21 -0700

    add images for shiny links

---
+++ shiny-link/test.R
@@ -0,0 +1,58 @@
+## Install and load needed libraries
+#install.packages("plotly")
+library(plotly)
+#install.packages("dplyr")
+library(dplyr)
+#install.packages("shiny")
+library(shiny)
+#install.packages("htmlwidgets")
+library(htmlwidgets)
+
+## Read in the fake csv data set about ice cream
+ice.cream <- read.csv("icecream-img.csv", stringsAsFactors = FALSE)
+
+ui <- fluidPage(
+  headerPanel("Price per Scoops"),
+  sidebarPanel(
+    sliderInput("priceRange", label = h3("Price Range"), min = 0, 
+                max = 10, value = c(4, 6))
+  ),
+  mainPanel(
+    plotlyOutput("icePlot"),
+    h4("Click on the dots to learn more about the ice cream flavor."),
+    uiOutput("imageLink"),
+    htmlOutput("picture")
+  )
+)
+
+server <- function(input, output) {
+  output$icePlot <- renderPlotly({
+    range.ice.cream <- ice.cream %>% filter(prices >= input$priceRange[1]) %>% filter(prices <= input$priceRange[2])
+    plot_ly(range.ice.cream, x = ~prices, y = ~scoops, type = "scatter", mode = "markers",
+            text = ~paste("Flavor:", flavors), key = ~images, source = "imgLink") %>% 
+      layout(title = paste("Ice Cream Price vs Scoops Given")) 
+  })
+  
+  output$imageLink <- renderText({
+    event.data <- event_data(event = "plotly_click", source = "imgLink")
+    if (is.null(event.data)) {
+      print("Click to see the link of the point.")
+    } else { 
+      ice.cream <- ice.cream %>% filter(images == event.data$key)
+      
+      HTML('<p>Flavor:',ice.cream$flavors, '</p>','<p>X Value:', event.data[["x"]], '</p>','<p>Y Value:', event.data[["y"]],'</p>',
+           '<a href="', ice.cream$images,'">', ice.cream$images,'</a>','<p>','</p>')
+      #print(ice.cream)
+    }
+  })
+  output$picture <- renderText({
+    event.data <- event_data(event = "plotly_click", source = "imgLink")
+    ice.cream <- ice.cream %>% filter(images == event.data$key)
+    link <- ice.cream$images
+    HTML('<img src ="', link, '">')
+      #print(ice.cream)
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)
